9e53c06a65ed0dc1a2d785b21837439de2b75e54
learnhtsd <160567527+learnhtsd@users.noreply.github.com>
2026-03-26 08:59:19 -0400

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,20 @@
+local BaseURL = "https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/"
+
+-- 1. Load the Library
+local Lib = loadstring(game:HttpGet(BaseURL .. "UI_Library.lua"))()
+
+-- 2. Load the Modules
+local MoveMod = loadstring(game:HttpGet(BaseURL .. "Modules/Movement.lua"))()
+local TeleMod = loadstring(game:HttpGet(BaseURL .. "Modules/Teleports.lua"))()
+
+-- 3. Assemble the UI
+local MoveTab = Lib:CreateTab("Movement")
+local TeleTab = Lib:CreateTab("Teleports")
+
+-- Add Movement Buttons
+Lib:CreateButton(MoveTab, "Speed (50)", function() MoveMod.SetSpeed(50) end)
+
+-- Add Teleport Buttons using a loop
+for _, loc in pairs(TeleMod.Locations) do
+    Lib:CreateButton(TeleTab, loc[1], function() TeleMod.GoTo(loc[2]) end)
+end
