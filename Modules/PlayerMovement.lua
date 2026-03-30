d73b026bd28b2fbc6ba4f588d6a2f17a4fe18ee4
learnhtsd <160567527+learnhtsd@users.noreply.github.com>
2026-03-30 17:41:27 -0400

    Update PlayerMovement.lua

--- Modules/PlayerMovement.lua
+++ Modules/PlayerMovement.lua
@@ -1,13 +1,15 @@
-local Module = {}
-
+-- Inside your module's Init function
 function Module.Init(Tab)
-    Tab:CreateSlider("WalkSpeed", 16, 250, 16, function(v)
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
-    end)
-    
-    Tab:CreateToggle("Infinite Jump", false, function(state)
+    Tab:CreateToggle("Speed Hack", false, function(state)
+        print("Speed is now: ", state)
         -- logic here
     end)
-end
 
-return Module -- <--- THIS LINE IS THE MOST IMPORTANT!
+    Tab:CreateSlider("WalkSpeed", 16, 200, 16, function(value)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+    end)
+
+    Tab:CreateKeybind("Quick Teleport", Enum.KeyCode.E, function()
+        print("E was pressed!")
+    end)
+end
