906c942199c4386b5716b6120e28854f42088211
learnhtsd <160567527+learnhtsd@users.noreply.github.com>
2026-03-30 16:45:50 -0400

    Update PlayerMovement.lua

--- Modules/PlayerMovement.lua
+++ Modules/PlayerMovement.lua
@@ -1,29 +1,27 @@
 local PlayerMovement = {}
 
 function PlayerMovement.Init(Api)
-    -- We use the API function from the Main script
+    -- Speed Toggle
+    _G.SpeedEnabled = false
     Api.CreateButton("Toggle Speed", function()
         _G.SpeedEnabled = not _G.SpeedEnabled
-        print("Speed is now: " .. tostring(_G.SpeedEnabled))
+        print("Speed is: " .. tostring(_G.SpeedEnabled))
     end)
 
+    -- Reset Button
     Api.CreateButton("Reset Character", function()
         if game.Players.LocalPlayer.Character then
             game.Players.LocalPlayer.Character:BreakJoints()
         end
     end)
 
-    -- Logic Loop
+    -- Speed Loop
     task.spawn(function()
         while task.wait() do
-            if _G.SpeedEnabled then
-                pcall(function()
-                    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50
-                end)
-            else
-                pcall(function()
-                    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
-                end)
+            local char = game.Players.LocalPlayer.Character
+            local hum = char and char:FindFirstChildOfClass("Humanoid")
+            if hum then
+                hum.WalkSpeed = _G.SpeedEnabled and 60 or 16
             end
         end
     end)
