7c398e0aed9a0df7b6ce76241b928da81c97ed7b
learnhtsd <160567527+learnhtsd@users.noreply.github.com>
2026-03-31 14:12:16 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local User = "learnhtsd"
 local Repo = "lt2"
 local Branch = "main" 
-local Version = "v0.0.067"
+local Version = "v0.0.068"
 
 -- ==========================================
 -- UI ENGINE START
