d8f88dc0e4842e941b9c17214da0d85b08ea08e7
learnhtsd <160567527+learnhtsd@users.noreply.github.com>
2026-04-06 10:11:04 -0400

    Update Home.lua

--- Modules/Home.lua
+++ Modules/Home.lua
@@ -1,30 +1,59 @@
 local HomeModule = {}
 
 function HomeModule.Init(Tab, Library)
-    -- Get the local player
     local Player = game:GetService("Players").LocalPlayer
-    local Username = Player.Name -- This is their @name
+    local TeleportService = game:GetService("TeleportService")
+    local Username = Player.Name 
 
-    -- 1. Welcome InfoBox (Updated with @username)
-    Tab:CreateInfoBox("Hello, " .. Username .. "!", "Thank you for using Dynxe. You are currently using a early version of Dynxe. Im alone and need time :)")
+    -- 1. Welcome InfoBox
+    Tab:CreateInfoBox("Hello, " .. Username .. "!", "Thank you for using Dynxe. You are currently using an early version of Dynxe. I'm alone and need time :)")
     
-    Tab:CreateInfoBox("Information", "Current Version: v~\nStatus: yup is working\nLast Updated: March 2026")
+    Tab:CreateInfoBox("Information", "Current Version: v0.0.101\nStatus: Online\nLast Updated: April 2026")
+
+    -- 2. SERVER MANAGEMENT SECTION
+    Tab:CreateSection("Server Management")
+
+    -- Helper function to ensure script auto-runs after teleporting
+    local function PrepareAutoLoad()
+        local scriptUrl = "https://raw.githubusercontent.com/learnhtsd/lt2/main/Main.lua" -- Use your actual loadstring URL
+        if queue_on_teleport then
+            queue_on_teleport('loadstring(game:HttpGet("' .. scriptUrl .. '"))()')
+        end
+    end
+
+    Tab:CreateAction("Rejoin Server", "Rejoin", function()
+        PrepareAutoLoad()
+        Library:Notify("System", "Rejoining current server...", 3)
+        TeleportService:Teleport(game.PlaceId, Player)
+    end)
+
+    Tab:CreateAction("Small Server", "Find New", function()
+        PrepareAutoLoad()
+        Library:Notify("System", "Searching for a different server...", 3)
+        
+        -- Basic server hop logic
+        local HttpService = game:GetService("HttpService")
+        local Servers = HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"))
+        
+        for _, s in pairs(Servers.data) do
+            if s.playing < s.maxPlayers and s.id ~= game.JobId then
+                TeleportService:TeleportToPlaceInstance(game.PlaceId, s.id, Player)
+                break
+            end
+        end
+    end)
 
     -- 3. Socials / Links Section
     Tab:CreateSection("Community & Support")
     
     Tab:CreateAction("Discord Server", "Copy Link", function()
-        setclipboard("https://discord.gg/yourlink") -- Replace with your actual link
+        setclipboard("https://discord.gg/yourlink")
         Library:Notify("System", "Discord link copied to clipboard!", 3)
     end)
 
     Tab:CreateAction("YouTube Channel", "Open Link", function()
-        -- Most executors support this to open a browser window
         if request then
-            request({
-                Url = "https://youtube.com/@yourchannel",
-                Method = "GET"
-            })
+            request({Url = "https://youtube.com/@yourchannel", Method = "GET"})
         else
             setclipboard("https://youtube.com/@yourchannel")
             Library:Notify("System", "Link copied (Executor doesn't support opening browsers)", 3)
