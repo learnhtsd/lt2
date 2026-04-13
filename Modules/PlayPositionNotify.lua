23abe4946b3fc588d131b299b553fe6bba48b424
learnhtsd <160567527+learnhtsd@users.noreply.github.com>
2026-04-13 17:29:42 -0400

    Update PlayPositionNotify.lua

--- Modules/PlayPositionNotify.lua
+++ Modules/PlayPositionNotify.lua
@@ -6,21 +6,21 @@ function PlayPositionNotify.Init(Tab, Library)
 
     Tab:CreateSection("Location Tools")
 
-    Tab:CreateAction("Get Position", "Check Coords & Copy", function()
+    Tab:CreateAction("Get Position", "Copy", function()
         local Character = Player.Character
         local Root = Character and Character:FindFirstChild("HumanoidRootPart")
 
         if Root then
             local Pos = Root.Position
             -- Formats to 1 decimal place and creates the string
-            local CoordString = string.format("X: %.1f, Y: %.1f, Z: %.1f", Pos.X, Pos.Y, Pos.Z)
+            local CoordString = string.format("%.1f, %.1f, %.1f", Pos.X, Pos.Y, Pos.Z)
             
             -- Copies the string to your Windows/Mac clipboard
             if setclipboard then
                 setclipboard(CoordString)
-                Library:Notify("Current Position", "Coordinates copied to clipboard!", 5)
+                Library:Notify("Current Position", "Coordinates Coppied: %.1f, %.1f, %.1f", 3)
             else
-                Library:Notify("Current Position", CoordString, 5)
+                Library:Notify("Current Position", CoordString, 3)
             end
         else
             Library:Notify("Error", "Character or RootPart not found!", 3)
