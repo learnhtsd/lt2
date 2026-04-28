2aaef9803d52685a5a6123caf4a176de8effa08d
learnhtsd <160567527+learnhtsd@users.noreply.github.com>
2026-04-28 09:56:52 -0400

    Update version from v0.0.185 to v0.0.186

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local User = "learnhtsd"
 local Repo = "lt2"
 local Branch = "main"
-local Version = "v0.0.185"
+local Version = "v0.0.186"
 
 -- ============================================================
 -- ██████╗  ██████╗ ███╗   ██╗███████╗██╗ ██████╗
