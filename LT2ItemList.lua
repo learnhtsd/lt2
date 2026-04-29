636b79dc6ab4f73f050bd7429545924cb4e59a2d
learnhtsd <160567527+learnhtsd@users.noreply.github.com>
2026-04-29 17:45:45 -0400

    Update LT2ItemList.lua

--- LT2ItemList.lua
+++ LT2ItemList.lua
@@ -1,3 +1,13 @@
 return {
-    { Name = "Basic Hatchet", Image = "BasicHatchet.png", Price = 12, BoxItemName = "BasicHatchet", },
+    -- Wood R Us
+    { Name = "Basic Hatchet", Image = "Axe_Basic_Hatchet.png", Price = 12, BoxItemName = "BasicHatchet", },
+    { Name = "Plain Axe", Image = "Axe_Plain.png", Price = 90, BoxItemName = "", },
+    { Name = "Steel Axe", Image = "Axe_Steel.png", Price = 190, BoxItemName = "", },
+    { Name = "Hardened Axe", Image = "Axe_Hardened.png", Price = 550, BoxItemName = "", },
+    { Name = "Silver Axe", Image = "Axe_Silver.png", Price = 2040, BoxItemName = "", },
+    { Name = "Straight Conveyor", Image = "Conveyor_Straight.png", Price = , BoxItemName = "", },
+    { Name = "Turn Conveyor", Image = "Conveyor_Turn.png", Price = , BoxItemName = "", },
+    --{ Name = "Conveyor", Image = "Conveyor_.png", Price = , BoxItemName = "", },
 }
+
+--{ Name = "", Image = ".png", Price = , BoxItemName = "", },
