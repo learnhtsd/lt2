d0eb321a2f4b2d0bb5a1b3a290a89e7bf69f6088
learnhtsd <160567527+learnhtsd@users.noreply.github.com>
2026-04-29 18:05:08 -0400

    Add new items to LT2ItemList.lua

--- LT2ItemList.lua
+++ LT2ItemList.lua
@@ -1,10 +1,11 @@
 return {
-    -- Wood R Us
+    -- Axes
     { Name = "Basic Hatchet", Image = "Axe_Basic_Hatchet.png", Price = 12, BoxItemName = "BasicHatchet", },
     { Name = "Plain Axe", Image = "Axe_Plain.png", Price = 90, BoxItemName = "", },
     { Name = "Steel Axe", Image = "Axe_Steel.png", Price = 190, BoxItemName = "", },
     { Name = "Hardened Axe", Image = "Axe_Hardened.png", Price = 550, BoxItemName = "", },
     { Name = "Silver Axe", Image = "Axe_Silver.png", Price = 2040, BoxItemName = "", },
+    -- Conveyors
     { Name = "Straight Conveyor", Image = "Conveyor_Straight.png", Price = 80, BoxItemName = "", },
     { Name = "Turn Conveyor", Image = "Conveyor_Turn.png", Price = 100, BoxItemName = "", },
     { Name = "Tilted Conveyor", Image = "Conveyor_Tilted.png", Price = 95, BoxItemName = "", },
@@ -15,6 +16,23 @@ return {
     { Name = "Sweeper Conveyor", Image = "Conveyor_Sweeper.png", Price = 430, BoxItemName = "", },
     { Name = "Conveyor Supports", Image = "Conveyor_Supports.png", Price = 12, BoxItemName = "", },
     { Name = "Turn Conveyor Suppords", Image = "Conveyor_Tilted_Supports.png", Price = 20, BoxItemName = "", },
+    -- Sawmills
+    { Name = "Shabby Sawmill", Image = "Sawmill_Shabby.png", Price = 130, BoxItemName = "", },
+    { Name = "Fair Sawmill", Image = "Sawmill_Fair.png", Price = 1600, BoxItemName = "", },
+    { Name = "Sawmax 01", Image = "Sawmax_01.png", Price = 11000, BoxItemName = "", },
+    { Name = "Sawmax 02", Image = "Sawmax_02.png", Price = 22500, BoxItemName = "", },
+    { Name = "Sawmax 02L", Image = "Sawmax_02L.png", Price = 86500, BoxItemName = "", },
+    { Name = "Chop Saw", Image = "Chop_Saw.png", Price = 12200, BoxItemName = "", },
+    -- Logic
+    { Name = "Wire", Image = "Wire.png", Price = 205, BoxItemName = "", },
+    { Name = "Button", Image = "Button.png", Price = 320, BoxItemName = "", },
+    { Name = "Lever", Image = "Lever.png", Price = 520, BoxItemName = "", },
+    { Name = "Pressure Plate", Image = "Pressure_Plate.png", Price = 640, BoxItemName = "", },
+    -- Vehicles
+    { Name = "Utility Truck", Image = "Utility_Truck.png", Price = 400, BoxItemName = "", },
+    -- Other
+    { Name = "Sandbag", Image = "Sandbag.png", Price = 1600, BoxItemName = "", },
+    { Name = "Worklight", Image = "Worklight.png", Price = 80, BoxItemName = "", },
 }
 
 --{ Name = "", Image = ".png", Price = , BoxItemName = "", },
