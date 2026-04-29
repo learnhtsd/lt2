03399d7bb050ad1214d4df8865b16eeffd80c070
learnhtsd <160567527+learnhtsd@users.noreply.github.com>
2026-04-29 19:32:32 +0000

    Update version number to v0.0.243

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local User = "learnhtsd"
 local Repo = "lt2"
 local Branch = "main"
-local Version = "v0.0.242"
+local Version = "v0.0.243"
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/learnhtsd/lt2/refs/heads/main/main.lua"))()
 
 -- ██████╗  ██████╗ ███╗   ██╗███████╗██╗ ██████╗
