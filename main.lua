59bacdf4f37a101addebb58b346abc35afdaffe6
learnhtsd <160567527+learnhtsd@users.noreply.github.com>
2026-05-02 11:36:21 -0400

    Bump version from v0.0.360 to v0.0.361

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local User = "learnhtsd"
 local Repo = "lt2"
 local Branch = "main"
-local Version = "v0.0.360"
+local Version = "v0.0.361"
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/learnhtsd/lt2/refs/heads/main/main.lua"))()
 
 -- ██████╗  ██████╗ ███╗   ██╗███████╗██╗ ██████╗
