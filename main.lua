a03ee268d5e79da468453e691ddd6e53660f4cc6
learnhtsd <160567527+learnhtsd@users.noreply.github.com>
2026-05-03 23:54:42 -0400

    Update version number to v0.0.404

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local User = "learnhtsd"
 local Repo = "lt2"
 local Branch = "main"
-local Version = "v0.0.403"
+local Version = "v0.0.404"
 
 task.spawn(function()
     local ICON_FOLDER  = "DynxeLT2"
